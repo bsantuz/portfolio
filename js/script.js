8b357087fd9323196d1b05aa9667a11256c12929
Bruno de Souza <109471488+bsantuz@users.noreply.github.com>
2022-10-07 23:08:20 -0300

    manuntenção

--- js/script.js
+++ js/script.js
@@ -1,96 +1,137 @@
-let quantExp = document.getElementById("container_exp").childElementCount
+let lowRes = false
+
+const navMenu = document.querySelector("#nav-menu")
+const quantExp = document.querySelector("#container_exp").childElementCount
+const carrousels = document.querySelector("#carousels")
 
 let scrolltemp = 0;
 
-const mql = window.matchMedia("(max-width: 780px)")
-
 let idxCarousel = 0;
 let quantShowing = 3;
-let widthMove = 250;
-let container = document.getElementById("container-projects");
-let countItens = container.childElementCount;
+const projectWidht = document.querySelector("#container-projects").children[0].clientWidth
+const projectLength = document.querySelector("#container-projects").children.length
 
-const cmql = window.matchMedia("(max-width: 1000px)")
 
-function setDisplayNone(el, hiden){
-    if(hiden){
+function ishiden(el)
+{ 
+    return el.style.display === "none" ? true : false 
+}
+
+function togledHiden(el, hiden = false)
+{ 
+    ishiden(el)? el.style.display = "flex" : el.style.display = "none" 
+}
+
+function islowRes()
+{
+   return window.innerWidth < 781 ? true : false
+}
+
+function buttonShowMenu()
+{
+    if (islowRes()) 
+    {
+        togledHiden(navMenu)  
+    }  
+}
+
+function ifNavBarhiden()
+{
+    if(!islowRes() && lowRes)
+    {
+        lowRes = false
+        if (ishiden(navMenu)) 
+        {
+            togledHiden(navMenu)
+        }
+    }
+    else if(islowRes() && !lowRes)
+    {
+        lowRes = true
+        togledHiden(navMenu)
+    }
+}
+
+function setDisplayNone(el, hiden)
+{
+    if(hiden)
+    {
         el.style.display  = hiden
-    }else{
-        if (el.style.display  != "none") {
+    }
+    else
+    {
+        if (el.style.display  != "none") 
+        {
             el.style.display  = "none"
-        }else{
+        }
+        else
+        {
             el.style.display  = "flex"
         }
     }
 }
 
-function buttonShowMenu(hiden){
-    let el = document.getElementById("nav-menu")
-
-    const mql = window.matchMedia("(max-width: 780px)")
-
-    if (mql.matches) {
-        if(hiden){
-            setDisplayNone(el, hiden)
-        }else{
-            setDisplayNone(el)
-        }
-    } 
-}
-
-function buttonExperiencies(id){
+function buttonExperiencies(id)
+{
     let el = document.getElementById(id)
     setDisplayNone(el)
 }
 
-function confOneCarousel(){
-    document.getElementById("container-projects").style.left = "0px"
-    document.getElementById("carousels").style.width = "250px"
-    widthMove = 200
-    quantShowing = 1;
-}
-
-//resulução maior que 1000px, alterar o left no sass para o site funcionar caso o js não funcione
-function confthreeCarousel(){
-    //document.getElementById("container-projects").style.left = "375px"
-    document.getElementById("carousels").style.width = "740px"
-    widthMove = 250
-    quantShowing = 3;
+function carouselForm()
+{
+    const carousel = document.getElementById("carousels")
+    if (window.innerWidth < 900) 
+    {
+        carousel.style.width = "250px"
+        quantShowing = 1;
+    }
+    else
+    {
+        carousel.style.width = "740px"
+        quantShowing = 3;
+    }
 }
 
 function carouselsNext(){
-    if (countItens - quantShowing > idxCarousel) {
-        idxCarousel ++;
-        let x = idxCarousel * -250;
-        container.style.transform = "translateX("+x+"px)";
+    if (carrousels.scrollLeft < projectLength * projectWidht ) {
+        carrousels.scrollLeft += projectWidht;
     }
 }
 
 function carouselsBack(){
-    if (0 < idxCarousel) {
-        idxCarousel --;
-        let x = idxCarousel * -250;
-        container.style.transform = "translateX("+x+"px)";
+    if (carrousels.scrollLeft > projectWidht) {
+        carrousels.scrollLeft -= projectWidht;
     }
 }
 
 function moveLabel(name){
-    document.getElementById(name).addEventListener("focus", () => {
-        document.getElementById("l"+name).style.top = "0";
+
+    const el = n => document.querySelector(`#${n}`)
+
+    el(name).addEventListener("focus", () => 
+    {
+        el("l"+name).style.top = "0";
     });
-    document.getElementById(name).addEventListener("blur", () => {
-        if(!document.getElementById(name).value){
-            document.getElementById("l"+name).style.color = "#FF4500"
-            document.getElementById("l"+name).style.top = "20px";
-        }else{
-            document.getElementById("l"+name).style.color = "#00FA9A"
+
+    el(name).addEventListener("blur", () => 
+    {
+        if(!el(name).value)
+        {
+            el("l"+name).style.color = "#FF4500"
+            el("l"+name).style.top = "15px";
+        }
+        else
+        {
+            el("l"+name).style.color = "#00FA9A"
         }
     });
-    document.getElementById("l"+name).style.top = "20px";
+
+    el("l"+name).style.top = "15px";
 }
 
 function copy(el){
-    document.getElementById(el).addEventListener("click", ()=>{
+    document.getElementById(el).addEventListener("click", ()=>
+    {
         navigator.clipboard.writeText(document.getElementById(el).innerText)
         alert("O texto copiado foi: " + document.getElementById(el).innerText);
     });
@@ -103,51 +144,33 @@ for (let index = 1; index <= quantExp +1; index++) {
     buttonExperiencies("p_exp0"+index)
 }
 
-window.addEventListener("scroll", function(event) {
-            
+window.addEventListener("scroll", function(event) 
+{          
     let scroll_y = this.scrollY;
     let menu = this.document.getElementById("menu-inicial")
 
-    if(scrolltemp > scroll_y ){
+    if(scrolltemp > scroll_y )
+    {
         scrolltemp = scroll_y
         menu.style.top = "0px"
     }
-    else{
+    else
+    {
         scrolltemp = scroll_y
         menu.style.top = "-99px"
     }
 
-    });
+});
 
-// mostra ou esconde os menus, baseado na resolução 780px
-if (cmql.matches) {
-    confOneCarousel()
-} else{
-    confthreeCarousel()
-}
+window.addEventListener('resize', () => {
 
-if (mql.matches) {
-    buttonShowMenu("none");
-} else{
-    buttonShowMenu("flex");
-}
+    ifNavBarhiden()
 
-mql.onchange = (e) => {
-    if (e.matches) {
-        buttonShowMenu("none");
-    } else{
-        buttonShowMenu("flex");
-    }
-}
+    carouselForm()
+});
 
-//muda a quantidade de projetos exibidos baseado na resolução 1000px
-cmql.onchange = (e) => {
-    if (e.matches) {
-        confOneCarousel()
-    } else{
-        confthreeCarousel()
-    }
-}
+ifNavBarhiden()
+carouselForm()
 
 moveLabel("name")
 moveLabel("subject")
@@ -155,3 +178,7 @@ moveLabel("email")
 
 copy("copyEmail")
 copy("copyTelefone")
+
+
+
+
